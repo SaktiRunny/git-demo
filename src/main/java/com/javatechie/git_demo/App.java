072d4e8167c85e55de5081f12572e715ecc0d92c
Sakti <ranjanbehera2033@gmail.com>
2023-06-05 12:37:40 +0530

    Checking by adding a method

---
+++ src/main/java/com/javatechie/git_demo/App.java
@@ -0,0 +1,20 @@
+package com.javatechie.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public int add(int a,int b,int c ) {
+		c=a+b;
+		return c;
+		}
+		
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
