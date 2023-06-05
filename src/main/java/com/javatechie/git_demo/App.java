3f68505ced52cbc44226c8f0b2fc6a286c70e9d5
Sakti <ranjanbehera2033@gmail.com>
2023-06-05 17:05:07 +0530

    Added a constant class

--- src/main/java/com/javatechie/git_demo/App.java
+++ src/main/java/com/javatechie/git_demo/App.java
@@ -1,9 +1,5 @@
 package com.javatechie.git_demo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	
@@ -11,6 +7,12 @@ public class App
 		c=a+b;
 		return c;
 		}
+	public void addData(String data) {
+		if(data.equals(Constant.DATA_TYPE)) {
+			System.out.println("Welcome to git-hub");
+		}
+		
+	}
 		
 	
     public static void main( String[] args )
